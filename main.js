d46abe5cee125e1c710c6021d01ae2d9918250fb
Robert <robert.mabry86@gmail.com>
2023-01-31 20:03:03 -0600

    g

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//makes the accordion...uh..accord
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -11,4 +12,25 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
+}
+//empty array for pushing new students
+const studs = [];
+
+//make the app
+const wizardTote = (divID, htmlpage) => {
+  const choseDiv = document.querySelector(divID);
+  choseDiv.innerHTML = htmlpage; 
+}
+
+//make the cards
+const domcards = (array) => {
+  let domString ="";
+  for (const whiz of array){
+    domString += `<div class="card" style="width: 18rem;">
+    <img src="${whiz.imageUrl}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">${whiz.name}</p>
+    </div>
+  </div>`
+  }
 }
