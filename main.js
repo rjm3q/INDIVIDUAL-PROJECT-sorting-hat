bd63d2de957391052e1bd9e41e4a3de12cd32f7f
Robert <robert.mabry86@gmail.com>
2023-02-03 22:48:01 -0600

    i saw the matrix, also my code twern't wurkin

--- main.js
+++ main.js
@@ -1,56 +1,21 @@
-//this func makes the form disappear
-function BerginSort(){
-  const SortHat = document.getElementById('WHOdini');
-  //gets current dispaly setting
-  var displaySetting = SortHat.style.display;
-  //grab the button to change text depending on state
-  var buttText = document.getElementById('start');
-  //if statment that toogles form
-  if (displaySetting == 'block'){
-    SortHat.style.display = 'none';
-    buttText.innerHTML = 'Click here to sort';
-  }
-  else{
-    SortHat.style.display = 'block';
-    buttText.innerHTML='Changed your mind muggle?'
-  }
-};
-
-//makes the accordion...uh..accord
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-//empty array for pushing new students
-const studs = [];
-
-//make the app
-const wizardTote = (divID, htmlpage) => {
+//starting over becuase i suck
+//dom rendering
+const domRender = (divID, htmlPage) =>{
   const choseDiv = document.querySelector(divID);
-  choseDiv.innerHTML = htmlpage; 
-}
-
-//make the cards
-const domcards = (array) => {
-  let domString ="";
-  for (const whiz of array){
-    domString += `<div class="card" style="width: 18rem;">
-    <img src="${whiz.imageUrl}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <p class="card-text">${whiz.name}</p>
-    </div>
-  </div>`
-  }
-  whizTote(".whizTote", domString)
+  choseDiv.inneraHTML = htmlPage;
 };
-domcards(studs)
+
+const skooncin = () =>{
+  let domString ="";
+  domString= `<div class="SortHat-card" id="SkoonCap">
+  <div class="SortHatBody">
+  <h1 class="SortHatTitle"></h1>
+  <p class="realHistory">"The Ilvermorny School of Witchcraft and Wizardry was founded in 1627, in order to escape teh tyranical rule
+of the puritans by use of magic. The orignal sorting hat resembled a capotain hat,
+however the personality of this magical item became too old fashioned for the school's 
+staff to manage as it spewed horrible demeaning words at the little witches and wizards. Thus,
+the headmaster of Ilvermorny at the time, David Crockett, imbued his famous coonskin cap with the
+ability to sort new students!"</p>
+  </div>
+  </div>`
+}
