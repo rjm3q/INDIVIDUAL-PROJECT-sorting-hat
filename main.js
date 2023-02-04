43fa515797c99b0de79cf88b53b90b4d8548e31c
Robert <robert.mabry86@gmail.com>
2023-02-04 09:06:02 -0600

    yeah, its a whole thing im working thru

--- main.js
+++ main.js
@@ -10,16 +10,18 @@ const domRender = (divID, htmlPage) =>{
 const skooncin = () =>{
   let domString ="";
   domString= `<div class="SortHat-card" id="SkoonCap">
-  <div class="SortHatBody">
-  <h1 class="SortHatTitle"></h1>
-  <p class="realHistory">"The Ilvermorny School of Witchcraft and Wizardry was founded in 1627, in order to escape teh tyranical rule
+  <div class="Sort-Hat-Body">
+  <h1 class="Sort-Hat-Title"></h1>
+  <p class="real-History">"The Ilvermorny School of Witchcraft and Wizardry was founded in 1627, in order to escape teh tyranical rule
 of the puritans by use of magic. The orignal sorting hat resembled a capotain hat,
 however the personality of this magical item became too old fashioned for the school's 
 staff to manage as it spewed horrible demeaning words at the little witches and wizards. Thus,
 the headmaster of Ilvermorny at the time, David Crockett, imbued his famous coonskin cap with the
 ability to sort new students!"</p>
+<a href="#" id="haus-picker" class="btn btn-primary">Pick a Yankee House</a>
   </div>
   </div>`
+domRender()
 };
 
 //gotta see the hat
@@ -27,14 +29,31 @@ const hatPic = () =>{
 
 };
 
+//build the form
+const studForm = () =>{
+  let domString="";
+  domString= ``
+  domRender("#studForm", domString)
+}
+
 //build students cards
 const hausbutts = () => {
-
+  let domString="";
+  domString=``
+  domRender("#hausbutts",domString)
 };
 
 //american house randomizer
 const HausofCards = (array, divID)=>{
-
+  let domString =``;
+  for (let i of array){
+    const hsHaus = i.haus === "Horned Serpent";
+    const wpHaus = i.haus === "Wampus";
+    const tbHaus = i.haus === "Thunderbird";
+    const pwHaus = i.haus === "Pukwudgie";
+    domString += ``
+  }
+  domRender(divID, domString)
 };
 
 //jerk patrol expulsion pile
@@ -49,7 +68,13 @@ const hausFilter = () =>{
 
 //start the sorting
 const SortMe = () =>{
-
+document.querySelector("#Skooncap").addEventListener("click", (e) => {
+  if(e.target.id === "haus-picker") {
+    document.querySelector("#haus-picker").style.display="none";
+    document.querySelector("#skoon-cap-img").style.display="none";
+    hausbutts()
+  }
+})
 };
 
 //good ol math. whatever
